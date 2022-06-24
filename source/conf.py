6973d614878d4d295dc66df44703e4477c586fbc
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-24 20:38:58 +0900

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,8 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx_gitstamp'
+    'sphinx_gitstamp', 
+    'sphinx_autodoc_typehints',
 ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
